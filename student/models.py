6684de63a3d26bf2d1668f0cb069edd2cc95aac1
niwedya9060@gmail.com <niwedya9060@gmail.com>
2025-08-30 13:35:22 +0530

    Added home_auth app

--- student/models.py
+++ student/models.py
@@ -17,14 +17,14 @@ class Parent(models.Model):
     mother_email = models.EmailField(max_length=100)
     present_address = models.TextField()
     permanent_address = models.TextField()
-    
+
     def __str__(self):
         return f"{self.father_name} & {self.mother_name}"
-    
+
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    student_id = models.CharField(max_length=20)
+    student_id = models.CharField(max_length=20, unique=True)
     gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'), ('Others', 'Others')])
     date_of_birth = models.DateField()
     student_class = models.CharField(max_length=50)
@@ -32,16 +32,15 @@ class Student(models.Model):
     joining_date = models.DateField()
     mobile_number = models.CharField(max_length=15)
     admission_number = models.CharField(max_length=20)
-    section = models.CharField(max_length=100)
+    section = models.CharField(max_length=10)
     student_email = models.EmailField(max_length=100)
     student_image = models.ImageField(upload_to='students/', blank=True)
     parent = models.OneToOneField(Parent, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(f"{self.first_name}-{self.last_name}-{self.student_id}")
         super(Student, self).save(*args, **kwargs)
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.student_id})"
-    
